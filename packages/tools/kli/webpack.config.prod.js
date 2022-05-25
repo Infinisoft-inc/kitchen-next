0a48fb236b88b8ebf474a54e87bfc3cf5ec7a563
Martin Ouimet <mouimet@infini-soft.com>
2022-05-24 22:54:49 -0400

    new kli build

---
+++ packages/tools/kli/webpack.config.prod.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright © All rights reserved 2022
+ * Infinisoft Inc.
+ * www.infini-soft.com
+ */
+
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: './src/index.js',
+  target: 'node',
+  mode: 'production',
+  output: {
+    path: path.join(process.cwd(), "dist"),
+    filename: 'kli.js',
+    library: "kli",
+    clean: true
+  },
+  resolve: {
+    cacheWithContext: false,
+    extensions: [".js", ".json"],
+    alias: {
+      '@/internals': path.resolve(__dirname, 'src/internals/'),
+      '@/commands': path.resolve(__dirname, 'src/commands/'),
+      '@/*': path.resolve(__dirname, './*'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)?$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env"
+            ]
+          }
+        },
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
