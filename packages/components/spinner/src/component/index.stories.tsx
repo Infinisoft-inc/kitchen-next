297b248970b603dc78761093c7424ed4c9c818d3
CayaCoco <mcaya@infini-soft.cloud>
2022-05-25 01:07:19 -0400

    New Kli spinner fader slider

---
+++ packages/components/spinner/src/component/index.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Spinner from '.';
+
+// https://storybook.js.org/docs/react/writing-stories/introduction
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'TEMPLATE/Spinner',
+  component: Spinner,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  // https://storybook.js.org/docs/react/essentials/controls
+
+} as ComponentMeta<typeof Spinner>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Spinner> = (args) => <Spinner {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+};
+
+
