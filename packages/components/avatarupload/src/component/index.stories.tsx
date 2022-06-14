1f7de867c01a5216f5c5717d41619b7defbaa845
CayaCoco <mcaya@infini-soft.cloud>
2022-06-14 00:15:07 -0400

    avatarupload storybook

--- packages/components/avatarupload/src/component/index.stories.tsx
+++ packages/components/avatarupload/src/component/index.stories.tsx
@@ -1,64 +1,46 @@
-// import { ComponentMeta, ComponentStory } from '@storybook/react';
-// import React from 'react';
-// import AvatarUpload from '.';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import AvatarUpload from '.';
+import { defaultAvatar } from './assets';
 
-// export default {
-//   title: 'TEMPLATE/AvatarUpload',
-//   component: AvatarUpload,
-//   argTypes: {
-//     content: {
-//       defaultValue: '',
-//       description: 'overwritten description',
-//       table: {
-//         category: 'Content',
-//         type: {
-//           summary: 'something short',
-//           detail: 'something really really long',
-//         },
-//         defaultValue: { summary: 'Hello' },
-//       },
-//       control: {
-//         type: null,
-//       },
-//     },
-//   },
-//   parameters: {
-//     docs: {
-//       description: {
-//         component: 'This is a super cool button',
-//       },
-//     },
-//   },
-// } as ComponentMeta<typeof AvatarUpload>;
+export default {
+  title: 'INPUT/AvatarUpload',
+  component: AvatarUpload,
+  argTypes: {
+    src: {
+      defaultValue: '',
+      description: '',
+      table: {
+        type: {
+          summary: `src: './'`,
+        },
+      },
+      control: {
+        type: 'file',
+      },
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: 'This is an Avatar uploader',
+      },
+    },
+  },
+} as ComponentMeta<typeof AvatarUpload>;
 
-// const Template: ComponentStory<typeof AvatarUpload> = (args) => (
-//   <AvatarUpload {...args}/>
-// );
+const Template: ComponentStory<typeof AvatarUpload> = (args) => (
+  <AvatarUpload {...args} />
+);
 
-// export const StringArgs = Template.bind({});
-// StringArgs.args = {
-//   title: 'Title',
-//   content: 'Content',
-// };
+export const SrcArgs = Template.bind({});
+SrcArgs.args = {
+  src: defaultAvatar,
+};
 
-// StringArgs.parameters = {
-//   docs: {
-//     description: {
-//       story: 'This is a string',
-//     },
-//   },
-// };
-
-// export const StringArgs2 = Template.bind({});
-// StringArgs2.args = {
-//   title: 'Title',
-//   content: 'Content',
-// };
-
-// StringArgs2.parameters = {
-//   docs: {
-//     description: {
-//       story: 'This is a string2',
-//     },
-//   },
-// };
+SrcArgs.parameters = {
+  docs: {
+    description: {
+      story: 'Avatar uploader',
+    },
+  },
+};
