9afabcd2fe7f3ffd81f11609d2be0d1224c1e135
Martin Ouimet <mouimet@infini-soft.com>
2022-06-15 23:58:26 -0400

    added steps to create

--- micro-apps/contact/src/components/create/content.tsx
+++ micro-apps/contact/src/components/create/content.tsx
@@ -6,6 +6,7 @@
 import React from 'react';
 import css from './index.module.css';
 import { Category } from './steps/category';
+import { Subcategory } from './steps/subcategory';
 
 const Step1 = React.lazy(() => import(/*webpackPreload: true*//* webpackChunkName: 'Step1' */ './steps/step1'))
 const Step2 = React.lazy(() => import(/*webpackPreload: true*//* webpackChunkName: 'Step2' */ './steps/step2'))
@@ -19,14 +20,15 @@ export const Content = ({ SK }: ContentProps) => {
   return <span key={SK}>
     <div className={css.content}>
       <Category hidden={step !== 1} SK={SK} />
-      <Step1 hidden={step !== 2} SK={SK} />
-      <Step2 hidden={step !== 3} SK={SK} />
+      <Subcategory hidden={step !== 2} SK={SK} />
+      <Step1 hidden={step !== 3} SK={SK} />
+      <Step2 hidden={step !== 4} SK={SK} />
     </div>
 
     <div className={css.footer}>
       <button hidden={step <= 1} className={css.next} onClick={() => setStep(prev => prev - 1)}>Back</button>
-      <button hidden={step === 3} className={css.next} onClick={() => setStep(prev => prev + 1)}>Next</button>
-      <button hidden={!(step === 3)} className={css.next} onClick={() => window.dispatchEvent(new CustomEvent('create.complete'))}>Complete</button>
+      <button hidden={step === 4} className={css.next} onClick={() => setStep(prev => prev + 1)}>Next</button>
+      <button hidden={!(step === 4)} className={css.next} onClick={() => {window.dispatchEvent(new CustomEvent('create.complete')); setStep(1)}}>Complete</button>
     </div>
   </span>
 
