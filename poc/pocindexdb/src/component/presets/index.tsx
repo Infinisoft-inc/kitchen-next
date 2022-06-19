94aa1a8b59eb27948458845718688d84262d3786
Martin Ouimet <mouimet@infini-soft.com>
2022-06-19 00:58:58 -0400

    indexdb poc

---
+++ poc/pocindexdb/src/component/presets/index.tsx
@@ -0,0 +1,12 @@
+/*
+ * Copyright © All rights reserved 2022
+ * Infinisoft Inc.
+ * www.infini-soft.com
+ */
+
+import { PocPocProps } from '../types'
+
+
+export type PocPocPresets = { }
+
+export const pocindexdbPresets: PocPocPresets = {}
